8065ea5f730c5a341866d2be64bf719e5b88a1a9
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-03-04 23:51:17 +0100

    update search input padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Component() {
               <div className='relative'>
                 <SearchIcon className='absolute left-2.5 top-2 h-4 w-4 text-gray-500 dark:text-gray-400' />
                 <input
-                  className='w-full bg-white shadow-none h-8 rounded appearance-none pl-8 md:w-2/3 lg:w-1/3 dark:bg-gray-950 outline-none'
+                  className='w-full bg-white shadow-none h-8 rounded appearance-none pl-8 pr-2 md:w-2/3 lg:w-1/3 dark:bg-gray-950 outline-none'
                   placeholder='Search products...'
                   type='search'
                 />
