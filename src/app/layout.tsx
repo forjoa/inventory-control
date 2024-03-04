d3317aa55e8fe2ee59fa6b8df7869f071cf5df6c
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-03-05 00:08:23 +0100

    delete unnecesary divs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,24 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <div className='grid min-h-screen w-full lg:grid-cols-[280px_1fr]'>
-          <div className='hidden border-r border-slate-600 bg-gray-100/40 lg:block dark:bg-gray-800/40'>
-            <div className='flex h-full max-h-screen flex-col gap-2'>
-              <div className='flex h-[60px] items-center border-b border-slate-600 px-6'>
-                <Link
-                  className='flex items-center gap-2 font-semibold'
-                  href='/'
-                >
-                  <Package2Icon className='h-6 w-6' />
-                  <span className=''>Store</span>
-                </Link>
-              </div>
-              <Navigation />
+      <body className={inter.className + ' grid min-h-screen w-full lg:grid-cols-[280px_1fr]'}>
+        <div className='hidden border-r border-slate-600 bg-gray-100/40 lg:block dark:bg-gray-800/40'>
+          <div className='flex h-full max-h-screen flex-col gap-2'>
+            <div className='flex h-[60px] items-center border-b border-slate-600 px-6'>
+              <Link className='flex items-center gap-2 font-semibold' href='/'>
+                <Package2Icon className='h-6 w-6' />
+                <span>Store</span>
+              </Link>
             </div>
+            <Navigation />
           </div>
-          {children}
         </div>
+        {children}
       </body>
     </html>
   )
