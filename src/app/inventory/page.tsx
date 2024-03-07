0222a64992aaa8ff361085a5510eec2d4a5cdb8b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-03-07 22:58:32 +0100

    delete unnecesary button

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -36,9 +36,6 @@ export default function Component() {
     <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6'>
       <div className='flex items-center'>
         <h1 className='font-semibold text-lg md:text-2xl'>Inventory</h1>
-        <button className='ml-auto bg-blue-500 px-5 py-2 rounded transition-all hover:bg-blue-400 text-white'>
-          Add product
-        </button>
       </div>
       <table className='w-auto'>
         <thead className='border border-slate-800 rounded-t'>
