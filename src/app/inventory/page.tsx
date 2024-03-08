43b61cd1e2011da3f489f3d94dabc843e54d020d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-03-08 12:45:06 +0100

    change inventory table styles

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -37,28 +37,36 @@ export default function Component() {
       <div className='flex items-center'>
         <h1 className='font-semibold text-lg md:text-2xl'>Inventory</h1>
       </div>
-      <table className='w-auto'>
-        <thead className='border border-slate-800 rounded-t'>
-          <tr>
-            <th className='p-3'>Name</th>
-            <th className='hidden md:table-cell p-3'>Status</th>
-            <th className='p-3'>Stock</th>
-            <th className='hidden md:table-cell p-3'>Company</th>
-          </tr>
-        </thead>
-        <tbody className='border border-slate-800 rounded-b'>
-          {products.map((value, index) => {
-            return (
-              <tr key={index}>
-                <td className='font-medium p-3'>{value.name}</td>
-                <td className='hidden md:table-cell p-3'>{value.status}</td>
-                <td className='p-3'>{value.stock}</td>
-                <td className='hidden md:table-cell p-3'>{value.company}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <div className="border border-slate-800 rounded-lg">
+        <table className='w-full'>
+          <thead className='border-b border-slate-800'>
+            <tr>
+              <th className='p-3 border-r border-slate-800'>NAME</th>
+              <th className='hidden md:table-cell p-3 border-r border-slate-800'>STATUS</th>
+              <th className='p-3 border-r border-slate-800'>STOCK</th>
+              <th className='hidden md:table-cell p-3 border-r border-slate-800'>COMPANY</th>
+              <th className='p-3'>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody className=''>
+            {products.map((value, index) => {
+              return (
+                <tr key={index} className="border-t border-slate-800">
+                  <td className='font-medium p-3 border-r border-slate-800'>{value.name}</td>
+                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>{value.status}</td>
+                  <td className='p-3 border-r border-slate-800'><p className="block m-auto w-fit">{value.stock}</p></td>
+                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>{value.company}</td>
+                  <td className='p-3 flex justify-center'>
+                    <button className='bg-blue-600 py-2 px-6 rounded'>
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
     </main>
   )
 }
