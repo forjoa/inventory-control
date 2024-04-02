201147016c99819184391966fe7bfb38e1d0cd2d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-02 11:55:25 +0200

    create file for types

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+interface Product {
+    product_id : number,
+    name : string,
+    description: string,
+    price : number,
+    cost : number,
+    size : string,
+    weight : number,
+    stock : number,
+    bar_code : string,
+    created_at : string,
+    category_id : number,
+    discount_id : number
+}
