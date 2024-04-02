84321044db1d14f7d6856336c3ec0e4da25bfbd8
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-02 20:20:28 +0200

    update enviroment variables name

--- src/database/database_connection.ts
+++ src/database/database_connection.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://bwilgcpoznydfosiupih.supabase.co'
-const supabaseKey = process.env.DB_KEY ?? ''
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseKey = process.env.NEXT_PUBLIC_DB_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey as string)
