11c96ec5bf7cfac4129d139d5de49ab3c9e588fe
forjoa <joaquin_trujillo@icloud.com>
2024-04-03 19:28:45 +0200

    delete button and edit button working correctly

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,28 +1,14 @@
 'use client'
-
 import { supabase } from '@/database/database_connection'
-import { ReactEventHandler, useState } from 'react'
 
 export default function DeleteButton({ product_id }: { product_id: number }) {
-  const [showPopup, setShowPopup] = useState<boolean>(false)
-  console.log(showPopup)
-
-  const togglePopup = (product_id:number) => {
-    setShowPopup(!showPopup)
-    deleteProduct(product_id)
-  }
-
   return (
-    <>
-      <button
-        className='bg-red-600 py-2 px-6 rounded transition-all hover:bg-red-500'
-        onClick={() => togglePopup(product_id)}
-      >
-        Delete
-      </button>
-
-      {showPopup && <Popup />}
-    </>
+    <button
+      className='bg-red-600 py-2 px-6 rounded transition-all hover:bg-red-500'
+      onClick={() => deleteProduct(product_id)}
+    >
+      Delete
+    </button>
   )
 }
 
@@ -30,29 +16,3 @@ async function deleteProduct(product_id: number) {
   await supabase.from('products').delete().eq('product_id', product_id)
 }
 
-const Popup = () => {
-    
-  return (
-    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
-      <div className='bg-white rounded-lg p-8'>
-        <button className='absolute top-0 right-0 p-2'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='M6 18L18 6M6 6l12 12'
-            />
-          </svg>
-        </button>
-        <p>This is a pop-up window!</p>
-      </div>
-    </div>
-  )
-}
--- src/components/EditButton.tsx
+++ src/components/EditButton.tsx
@@ -1,11 +1,49 @@
 'use client'
-
 import { Product } from "@/types/types"
+import { useState } from "react"
 
-export default function EditButton({ product } : { product: Product}) {
+export default function EditButton({ product }: { product: Product }) {
+  const [showPopup, setShowPopup] = useState<boolean>(false)
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup)
+  }
+
   return (
-    <button className='bg-blue-600 py-2 px-6 rounded transition-all hover:bg-blue-500' onClick={() => console.log(product)}>
-      Edit
-    </button>
+    <>
+      <button className='bg-blue-600 py-2 px-6 rounded transition-all hover:bg-blue-500' onClick={togglePopup}>
+        Edit
+      </button>
+      {showPopup && <EditProductFormPopup product={product} onClose={togglePopup}/>}
+    </>
+  )
+}
+
+
+const EditProductFormPopup = ({ product, onClose }: { product: Product, onClose: any }) => {
+
+  return (
+    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+      <div className='border border-slate-800 bg-gray-100/40 lg:block dark:bg-gray-800/40 ml-5 my-5 rounded-lg'>
+        <button className='absolute top-0 right-0 p-2'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            onClick={onClose}
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M6 18L18 6M6 6l12 12'
+            />
+          </svg>
+        </button>
+        <p>This is a pop-up window!</p>
+      </div>
+    </div>
   )
 }
