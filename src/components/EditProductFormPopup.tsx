dd9ef2c4b8f8aa609038dd417a73a13c5f4284e2
forjoa <joaquin_trujillo@icloud.com>
2024-04-04 17:29:26 +0200

    add variables to handle inputs change

--- src/components/EditProductFormPopup.tsx
+++ src/components/EditProductFormPopup.tsx
@@ -1,6 +1,36 @@
+import React, { useState } from 'react'
 import { Product } from "@/types/types"
 
 const EditProductFormPopup = ({ product, onClose }: { product: Product, onClose: any }) => {
+    const [formData, setFormData] = useState<Product>({
+        product_id: product.product_id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        cost: product.cost,
+        size: product.size,
+        weight: product.weight,
+        stock: product.stock,
+        bar_code: product.bar_code,
+        created_at: product.created_at,
+        category_id: product.category_id,
+        discount_id: product.discount_id
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(formData)
+        onClose() 
+    }
+
     return (
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
             <div className='border border-slate-800 w-1/4 bg-gray-100 lg:block dark:bg-gray-900 rounded-lg'>
@@ -23,40 +53,40 @@ const EditProductFormPopup = ({ product, onClose }: { product: Product, onClose:
                 </button>
                 <div className="px-8 pb-8 w-full">
                     <p className="text-xl font-bold">Form to edit a product</p><br />
-                    <form className="w-full flex flex-col gap-1">
+                    <form className="w-full flex flex-col gap-1" onSubmit={handleSubmit}>
                         <label htmlFor="name">Name:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="text" id="name" name="name" value={product.name} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
 
                         <label htmlFor="description">Description:</label>
-                        <textarea className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" id="description" name="description" value={product.description}></textarea>
+                        <textarea className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" id="description" name="description" value={formData.description} onChange={handleChange}></textarea>
 
                         <label htmlFor="price">Price:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="price" name="price" value={product.price} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="price" name="price" value={formData.price} onChange={handleChange} />
 
                         <label htmlFor="cost">Cost:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="cost" name="cost" value={product.cost} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="cost" name="cost" value={formData.cost} onChange={handleChange} />
 
                         <label htmlFor="size">Size:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="text" id="size" name="size" value={product.size} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="text" id="size" name="size" value={formData.size} onChange={handleChange} />
 
                         <label htmlFor="weight">Weight:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="weight" name="weight" value={product.weight} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="weight" name="weight" value={formData.weight} onChange={handleChange} />
 
                         <label htmlFor="stock">Stock:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="stock" name="stock" value={product.stock} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="stock" name="stock" value={formData.stock} onChange={handleChange} />
 
                         <label htmlFor="bar_code">Bar Code:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="text" id="bar_code" name="bar_code" value={product.bar_code} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="text" id="bar_code" name="bar_code" value={formData.bar_code} onChange={handleChange} />
 
                         <label htmlFor="category_id">Category ID:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="category_id" name="category_id" value={product.category_id} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="category_id" name="category_id" value={formData.category_id} onChange={handleChange} />
 
                         <label htmlFor="discount_id">Discount ID:</label>
-                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="discount_id" name="discount_id" value={product.discount_id} />
+                        <input className="bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none" spellCheck="false" type="number" id="discount_id" name="discount_id" value={formData.discount_id} onChange={handleChange} />
 
                         <br />
 
-                        <input type="submit" value="Submit" className="bg-blue-500 rounded py-2 px-4" />
+                        <input type="submit" value="Submit" className="bg-blue-500 rounded py-2 px-4 cursor-pointer" />
                     </form>
 
                 </div>
