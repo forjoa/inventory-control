4cab2ba7fb56de5c98ed3e496cfedc940bc69e76
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-09 10:35:13 +0200

    login to auth working correctly

--- src/components/ProtectedComponent.tsx
+++ src/components/ProtectedComponent.tsx
@@ -4,17 +4,22 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { supabase } from '@/database/database_connection'
 
-const ProtectedRoute = ({ children } : { children: any}) => {
+const ProtectedRoute = ({ children }: { children: any }) => {
   const router = useRouter()
 
-  useEffect(() => {
-    const session = supabase.auth.getSession()
-    if (!session) {
+  const auth = async () => {
+    const session = await supabase.auth.getSession()
+    if (!session.data.session) {
       router.push('/login')
     }
-  }, [router])
+  }
+
+  useEffect(() => {
+    auth()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return <>{children}</>
-};
+}
 
 export default ProtectedRoute
