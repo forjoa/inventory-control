11b4f7217e870df7ca388da87326a3be1464dc56
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-09 10:29:48 +0200

    add Protected Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import ProtectedRoute from '@/components/ProtectedComponent'
+
 export default function Component() {
   return (
-    <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6'>
-      <div className='flex items-center'>
-        <h1 className='font-semibold text-lg md:text-2xl'>Analytics</h1>
-      </div>
-    </main>
+    <ProtectedRoute>
+      <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6'>
+        <div className='flex items-center'>
+          <h1 className='font-semibold text-lg md:text-2xl'>Analytics</h1>
+        </div>
+      </main>
+    </ProtectedRoute>
   )
 }
