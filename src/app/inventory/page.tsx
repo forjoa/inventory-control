6ca8adaf193454b62b568d4b1b26540f8e83d2c3
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-09 11:33:23 +0200

    change mq for inventory table

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -19,8 +19,8 @@ export default async function Component() {
   return (
     <ProtectedRoute>
       <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6'>
-        <div className='w-full flex justify-between'>
-          <h1 className='font-semibold text-lg md:text-2xl'>Inventory</h1>
+        <div className='w-full flex justify-between align-middle'>
+          <h1 className='font-semibold text-lg md:text-2xl flex align-middle'>Inventory</h1>
           <div className='flex gap-2'>
             <AddProductButton />
             <AddCategoryButton />
@@ -32,26 +32,26 @@ export default async function Component() {
             <thead className='border-b border-slate-800'>
               <tr>
                 <th className='p-3 border-r border-slate-800'>NAME</th>
-                <th className='hidden md:table-cell p-3 border-r border-slate-800'>
+                <th className='hidden lg:table-cell p-3 border-r border-slate-800'>
                   DESCRIPTION
                 </th>
                 <th className='p-3 border-r border-slate-800'>PRICE</th>
-                <th className='hidden md:table-cell p-3 border-r border-slate-800'>
+                <th className='hidden lg:table-cell p-3 border-r border-slate-800'>
                   COST
                 </th>
-                <th className='hidden md:table-cell p-3 border-r border-slate-800'>
+                <th className='hidden lg:table-cell p-3 border-r border-slate-800'>
                   SIZE
                 </th>
-                <th className='hidden md:table-cell p-3 border-r border-slate-800'>
+                <th className='hidden lg:table-cell p-3 border-r border-slate-800'>
                   WEIGHT
                 </th>
-                <th className='hidden md:table-cell p-3 border-r border-slate-800'>
+                <th className='hidden lg:table-cell p-3 border-r border-slate-800'>
                   STOCK
                 </th>
-                <th className='hidden md:table-cell p-3 border-r border-slate-800'>
+                <th className='hidden lg:table-cell p-3 border-r border-slate-800'>
                   BAR CODE
                 </th>
-                <th className='hidden md:table-cell p-3 border-r border-slate-800'>
+                <th className='hidden lg:table-cell p-3 border-r border-slate-800'>
                   CREATED AT
                 </th>
                 <th className='p-3'>ACTIONS</th>
@@ -63,28 +63,28 @@ export default async function Component() {
                   <td className='font-medium p-3 border-r border-slate-800'>
                     {product.name}
                   </td>
-                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>
+                  <td className='hidden lg:table-cell p-3 border-r border-slate-800'>
                     {product.description}
                   </td>
                   <td className='p-3 border-r border-slate-800'>
                     {`${product.price} $`}
                   </td>
-                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>
+                  <td className='hidden lg:table-cell p-3 border-r border-slate-800'>
                     {`${product.cost} $`}
                   </td>
-                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>
+                  <td className='hidden lg:table-cell p-3 border-r border-slate-800'>
                     {product.size}
                   </td>
-                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>
+                  <td className='hidden lg:table-cell p-3 border-r border-slate-800'>
                     {`${product.weight} kg`}
                   </td>
-                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>
+                  <td className='hidden lg:table-cell p-3 border-r border-slate-800'>
                     {`${product.stock} u`}
                   </td>
-                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>
+                  <td className='hidden lg:table-cell p-3 border-r border-slate-800'>
                     {product.bar_code}
                   </td>
-                  <td className='hidden md:table-cell p-3 border-r border-slate-800'>
+                  <td className='hidden lg:table-cell p-3 border-r border-slate-800'>
                     {formatDate(product.created_at)}
                   </td>
                   <td className='py-1 flex justify-center gap-3'>
