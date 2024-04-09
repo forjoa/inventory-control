9e552860e93cc8321645eebb0a91d174c7ae3839
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-09 11:27:21 +0200

    login path w/ styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { supabase } from '@/database/database_connection'
 
@@ -11,7 +11,10 @@ const LoginPage = () => {
   const handleLogin = async (e: any) => {
     e.preventDefault()
     try {
-      const { data, error } = await supabase.auth.signInWithPassword({ email, password })
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      })
       if (error) {
         throw error
       }
@@ -23,15 +26,37 @@ const LoginPage = () => {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
+    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+      <div className='border border-slate-800 w-3/4 md:w-1/4 bg-gray-100 lg:block dark:bg-gray-900 rounded-lg'>
+        <div className='p-8 w-full'>
+          <form onSubmit={handleLogin} className='flex flex-col gap-2'>
+            <h1 className='text-lg font-bold'>Login to use the dashboard</h1>
+            <input
+              className='bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none'
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className='bg-gray-100/40 dark:bg-gray-800/40 rounded p-2 outline-none'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-export default LoginPage;
+            <button
+              type='submit'
+              className='bg-blue-600 py-2 px-6 rounded transition-all hover:bg-blue-500 '
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
