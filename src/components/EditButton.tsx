c7e1debe85cdbdc1af98229f59ca06003ce65818
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-13 09:33:09 +0200

    add dark and bright mq for edit button

--- src/components/EditButton.tsx
+++ src/components/EditButton.tsx
@@ -12,7 +12,7 @@ export default function EditButton({ product }: { product: Product }) {
 
   return (
     <>
-      <button className='bg-blue-600 py-2 px-6 rounded transition-all hover:bg-blue-500' onClick={togglePopup}>
+      <button className='bg-blue-600 py-2 px-6 rounded transition-all hover:bg-blue-500 text-white dark:text-gray-800' onClick={togglePopup}>
         Edit
       </button>
       {showPopup && <EditProductFormPopup product={product} onClose={togglePopup}/>}
