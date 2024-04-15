29c239ee67ccdca62619188eb1fbb990d49cb73e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-15 17:07:42 +0200

    update new add discount icon

--- src/components/AddDiscountButton.tsx
+++ src/components/AddDiscountButton.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Add } from "@/assets/icons";
+import { AddDiscountIcon } from "@/assets/icons";
 import { useState } from "react";
 import AddNewDiscountPopup from "./AddNewDiscountPopup";
 
@@ -13,7 +13,7 @@ export default function AddDiscountButton() {
     return (
         <>
             <button className='flex gap-2 py-2 px-4 border border-slate-800 bg-gray-100/40 dark:bg-gray-800/40 rounded-lg' onClick={togglePopup}>
-                <Add />
+                <AddDiscountIcon />
                 <p className="hidden md:block">
                     Discount
                 </p>
