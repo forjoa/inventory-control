969f3a471e24c0f1417c692a227244419ede3038
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-15 21:51:57 +0200

    change title from home

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,8 +2,13 @@ import { Package2Icon } from '@/assets/icons'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import './globals.css'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Store | Home',
+}
 
 export default function Layout({ children }: { children: any }) {
   return (
