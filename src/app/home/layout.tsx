9b68458e7490810bc0d0597bdf3d731a6a306e98
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-15 19:09:30 +0200

    started home page

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,68 @@
+import { Package2Icon } from '@/assets/icons'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Layout({ children }: { children: any }) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <div className='flex flex-col min-h-[100dvh] max-w-[80dvw] mx-auto'>
+          <header className='h-14 flex items-center px-4 md:px-6'>
+            <div className='hidden md:flex items-center gap-4 lg:gap-6'>
+              <Link className='flex items-center gap-2 font-semibold' href='/home'>
+                <Package2Icon className='h-6 w-6' />
+                <span className=''>Store</span>
+              </Link>
+            </div>
+            <div className='w-full md:flex-1'>
+              <form>
+                <input
+                  className='hidden md:w-[300px] lg:w-[400px] xl:w-[500px] md:flex md:ml-4 md:px-2 md:py-1 lg:ml-6 rounded-lg outline-none dark:text-black text-sm'
+                  placeholder='Search products...'
+                  type='search'
+                />
+              </form>
+            </div>
+            <div className='flex ml-auto lg:ml-0 items-center gap-4 lg:gap-6'>
+              <Link
+                className='hidden md:flex text-sm font-medium hover:underline underline-offset-4'
+                href='#'
+              >
+                Login
+              </Link>
+              <Link
+                className='hidden md:flex text-sm font-medium hover:underline underline-offset-4'
+                href='#'
+              >
+                Register
+              </Link>
+              <button className='dark:text-black dark:bg-white text-sm rounded-md px-3 py-1'>Cart</button>
+            </div>
+          </header>
+          {children}
+          <footer className='flex flex-col gap-2 sm:flex-row py-4 w-full shrink-0 items-center px-4 md:px-6'>
+            <p className='text-xs text-gray-500 dark:text-gray-400'>
+              © 2024 Store. All rights reserved.
+            </p>
+            <nav className='sm:ml-auto flex gap-4 sm:gap-6'>
+              <Link
+                className='text-xs hover:underline underline-offset-4'
+                href='#'
+              >
+                Terms of Service
+              </Link>
+              <Link
+                className='text-xs hover:underline underline-offset-4'
+                href='#'
+              >
+                Privacy
+              </Link>
+            </nav>
+          </footer>
+        </div>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,89 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { supabase } from '@/database/database_connection'
+import { Discount, Product } from '@/types/types'
+
+export default async function Component() {
+  const { data } = await supabase
+    .from('products')
+    .select(
+      'product_id, name, description, price, cost, size, weight, stock, bar_code, created_at, discount_id, category_id'
+    )
+  const products: Product[] = data as Product[]
+
+  return (
+    <main className='grid gap-6 p-4 lg:gap-12 lg:p-6'>
+      <div className='grid gap-4'>
+        <div className='grid gap-2'>
+          <h1 className='text-2xl font-bold tracking-tighter lg:text-4xl'>
+            Summer Collection
+          </h1>
+          <p className='text-gray-500 dark:text-gray-400'>
+            Shop the latest trends
+          </p>
+        </div>
+        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+          {products.map(async (product, index) => {
+            const { data } = await supabase
+              .from('discounts')
+              .select('*')
+              .eq('discount_id', product.discount_id)
+            const discount: Discount = data != null ? data[0] : {}
+
+            return (
+              <div
+                key={index}
+                className='border border-slate-800 rounded-lg relative'
+              >
+                <span
+                  className={`${
+                    product.discount_id > 0
+                      ? 'absolute top-[-10px] right-[-10px] bg-red-500 px-1 rounded'
+                      : 'hidden'
+                  }`}
+                >
+                  Discounted product!
+                </span>
+                <Link
+                  className='aspect-square overflow-hidden rounded-lg'
+                  href='#'
+                >
+                  <Image
+                    alt={product.name}
+                    className='object-cover aspect-image rounded-t-lg'
+                    height='500'
+                    src='/placeholder.svg'
+                    width='500'
+                  />
+                  <span className='sr-only'>View item</span>
+                </Link>
+                <div className='p-4'>
+                  <Link
+                    className='font-bold line-clamp-2 hover:underline underline-offset-2'
+                    href='#'
+                  >
+                    {product.name}
+                  </Link>
+                  <span className='font-light'>{product.description}</span>
+                  <p className='font-semibold'>
+                    <span>
+                      {' '}
+                      {Math.floor(
+                        product.price -
+                          product.price * (discount.discount_percentage / 100)
+                      )}{' '}
+                      €
+                    </span>
+                    <br />
+                    <del className='text-slate-500'>{product.price} €</del>{' '}
+                    <span className='text-red-400 text-[13px]'>-{discount.discount_percentage}%{' '}</span>
+                  </p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </main>
+  )
+}
