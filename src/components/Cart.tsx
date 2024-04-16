3cc7fc9063b7c296b17197b497c7c89c6f16acd7
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-16 17:35:21 +0200

    give gap to cart content

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,17 +6,22 @@ import { CloseIcon } from '@/assets/icons'
 const Cart = ({ handleToggle }: { handleToggle: any }) => {
   const { cart, removeFromCart } = useContext(CartContext)
 
-  const totalAmount = cart.reduce((total, product) => total + product.price, 0);
+  const totalAmount = cart.reduce((total, product) => total + product.price, 0)
 
   return (
     <div className='absolute bg-black border border-gray-800 rounded text-white p-4 top-[60px] right-[130px] z-10 flex flex-col gap-2'>
-      <span onClick={handleToggle} className='w-full flex justify-end'><CloseIcon width={16}/></span>
+      <span onClick={handleToggle} className='w-full flex justify-end'>
+        <CloseIcon width={16} />
+      </span>
       <p className='w-full flex'>Cart</p>
-      <div>
+      <div className='flex flex-col gap-2'>
         <ul className='flex flex-col gap-2'>
           {cart.map((product, index) => {
             return (
-              <li key={index} className='flex justify-between align-middle gap-4'>
+              <li
+                key={index}
+                className='flex justify-between align-middle gap-4'
+              >
                 <div className='flex flex-col text-left'>
                   <p>{product.name}</p>
                   <p className='text-slate-400'>{product.price} €</p>
@@ -26,14 +31,14 @@ const Cart = ({ handleToggle }: { handleToggle: any }) => {
                     className='bg-red-500 rounded py-2 px-4 hover:bg-red-400 transition-all my-auto'
                     onClick={() => removeFromCart(product.product_id)}
                   >
-                    <CloseIcon width={14} height={14}/>
+                    <CloseIcon width={14} height={14} />
                   </button>
                 </div>
               </li>
             )
           })}
         </ul>
-        <hr className='text-gray-800'/>
+        <hr className='text-gray-800' />
         <div className='flex justify-between'>
           <p>Total:</p>
           <p>{totalAmount} €</p>
