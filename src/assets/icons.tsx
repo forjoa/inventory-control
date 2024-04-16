21beb689629da9192b3d770c18e9038500ebbdf1
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-16 17:24:20 +0200

    update close icon

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -265,8 +265,6 @@ export function CloseIcon(props: any) {
     <svg
       {...props}
       xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
