9b1f7b3a9f9db85a679c7eabf289c2970563c8ba
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-17 09:39:12 +0200

    create and separate footer component

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from 'next'
 import CartButton from '@/components/CartButton'
 import { CartProvider } from '@/context/CartContext'
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,25 +22,7 @@ export default function Layout({ children }: { children: any }) {
           <div className='flex flex-col min-h-[100dvh] max-w-[80dvw] mx-auto'>
             <Header />
             {children}
-            <footer className='flex flex-col gap-2 sm:flex-row py-4 w-full shrink-0 items-center px-4 md:px-6'>
-              <p className='text-xs text-gray-500 dark:text-gray-400'>
-                © 2024 Store. All rights reserved.
-              </p>
-              <nav className='sm:ml-auto flex gap-4 sm:gap-6'>
-                <Link
-                  className='text-xs hover:underline underline-offset-4'
-                  href='#'
-                >
-                  Terms of Service
-                </Link>
-                <Link
-                  className='text-xs hover:underline underline-offset-4'
-                  href='#'
-                >
-                  Privacy
-                </Link>
-              </nav>
-            </footer>
+            <Footer />
           </div>
         </body>
       </html>
