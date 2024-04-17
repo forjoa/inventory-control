b1edf3ea501321c3b280c87fa9e4f00fbb4633b0
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-17 09:49:18 +0200

    add cart lenght to cart button

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,21 +1,30 @@
 'use client'
 import { AddProduct } from '@/assets/icons'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import Cart from './Cart'
+import { CartContext } from '@/context/CartContext'
 
 export default function CartButton() {
+  const { cart } = useContext(CartContext)
   const [togglePopup, setTogglePopup] = useState<boolean>(false)
 
   const handleToggle = () => setTogglePopup(!togglePopup)
 
   return (
+    <>
       <button
-        className='dark:text-black dark:bg-white text-sm rounded-md px-3 py-1 flex justify-center gap-1'
+        className='dark:text-black dark:bg-white text-sm rounded-md px-3 py-1 flex justify-center items-center gap-1'
         onClick={handleToggle}
       >
         <AddProduct />
-        <span className='hidden md:block md:h-full my-auto'>Cart</span>
-        {togglePopup && <Cart handleToggle={handleToggle} />}
+        <p className='hidden md:flex md:justify-center md:items-center md:h-full my-auto'>
+          Cart{' '}
+          <span className='dark:text-white dark:bg-black flex justify-center items-center px-2 rounded-full aspect-square ml-1'>
+            {cart.length}
+          </span>
+        </p>
       </button>
+      {togglePopup && <Cart handleToggle={handleToggle} />}
+    </>
   )
 }
